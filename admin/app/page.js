1825c38881bf59cfd85f6976314ca3d6332c8aae
Shinzo27 <20bmiit031@gmail.com>
2024-09-13 21:53:02 +0530

    Admin

--- admin/app/page.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="bg-black">
-      <h1 className="text-3xl font-bold text-white">Admin Panel for Patel's Grocery Store</h1>
-    </div>
-  );
-}
