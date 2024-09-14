6e5d3aebade13fa7dac2350eaed841e971b718a7
Shinzo27 <20bmiit031@gmail.com>
2024-09-14 18:44:31 +0530

    Changes

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -4,7 +4,7 @@ import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
 export const metadata: Metadata = {
   title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
+    "Patel's Dryfruit And Masala",
   description: "This is Next.js Home for TailAdmin Dashboard Template",
 };
 
