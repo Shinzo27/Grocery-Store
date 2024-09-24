c36799bc92b88c0eb1f6f71c742fb1d685b089cc
Shinzo27 <20bmiit031@gmail.com>
2024-09-24 09:01:46 +0530

    signin

--- admin/src/app/api/auth/[...nextAuth]/route.ts
+++ admin/src/app/api/auth/[...nextAuth]/route.ts
@@ -1,28 +1,39 @@
 import axios from "axios";
-import NextAuth from "next-auth/next"; 
+import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { pages } from "next/dist/build/templates/app-page";
 
 export default NextAuth({
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: { label: "Email", type: "email", placeholder: "Email" },
-                password: { label: "Password", type: "password", placeholder: "Password" }
-            },
-            authorize: async (credentials) => {
-                const { data } = await axios.post('http://localhost:8000/api/v1/user/admin/signin', { email: credentials?.email, password: credentials?.password }, { withCredentials: true });
-                if(data.success){
-                    return {
-                        id: data.user.id,
-                        name: data.user.username,
-                        email: data.user.email,
-                    }
-                }
-                else{
-                    return null;
-                }
-            }
-        }),
-    ] 
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      authorize: async (credentials) => {
+        const { data } = await axios.post(
+          "http://localhost:8000/api/v1/user/admin/signin",
+          { email: credentials?.email, password: credentials?.password },
+          { withCredentials: true },
+        );
+        if (data.success) {
+          return {
+            id: data.user.id,
+            name: data.user.username,
+            email: data.user.email,
+          };
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+  },
 });
