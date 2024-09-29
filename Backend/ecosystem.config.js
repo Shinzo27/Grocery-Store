3c33b872d608c920f362ae4f20825132254d7fcd
Shinzo27 <20bmiit031@gmail.com>
2024-09-29 12:42:03 +0530

    deploy

---
+++ Backend/ecosystem.config.js
@@ -0,0 +1,12 @@
+export const apps = [{
+    name: "Grocery Store",
+    script: "./index.js",
+    instances: "max",
+    exec_mode: "cluster",
+    env: {
+        NODE_ENV: "development",
+    },
+    env_production: {
+        NODE_ENV: "production",
+    },
+}];
